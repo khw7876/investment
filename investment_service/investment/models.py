b119e9560b4e9a49742d074a0c061f2931e97ed9
khw7876 <skanahrwi@naver.com>
2022-09-19 20:49:04 +0900

    feat/ csv 파일을 토대로 user, bank 데이터 생성

--- investment_service/investment/models.py
+++ investment_service/investment/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Bank(models.Model):
+    name = models.CharField("증권사 명", max_length=40)
+
+    def __str__(self):
+        return self.name
