e73326e644b0e83f13cd2826ce14d4277d01b680
khw7876 <skanahrwi@naver.com>
2022-09-20 23:14:43 +0900

    refac/ model PositiveInteger 재설정

--- investment_service/investment/models.py
+++ investment_service/investment/models.py
@@ -19,20 +19,6 @@ class Investment(models.Model):
     def __str__(self):
         return self.user, self.account_name
 
-class InvestmentHistory(models.Model):
-    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
-    account_name = models.CharField("계좌명", max_length=40)
-    account_num = models.IntegerField("계좌번호")
-    starting_fund = models.IntegerField("투자원금", default=0)
-    total_asset = models.IntegerField("계좌 총 자산", default=0)
-    isin = models.CharField("ISIN", max_length=40)
-    cur_price = models.IntegerField("현재가")
-    order = models.IntegerField("보유수량")
-
-    def __str__(self):
-        return self.account_name, self.cur_price, self.order
-
 class AssetGroup(models.Model):
     name = models.CharField("자산그룹명", max_length=30)
 
@@ -41,8 +27,23 @@ class AssetGroup(models.Model):
 
 class Stock(models.Model):
     name = models.CharField("자산그룹명", max_length=30)
-    current_price = models.IntegerField("현재가")
-    isin = models.CharField("ISIN", max_length=40)
+    current_price = models.PositiveIntegerField("현재가", default=0)
+    isin = models.CharField("ISIN", max_length=40, primary_key=True)
 
     def __str__(self):
         return self.name
+
+class InvestmentHistory(models.Model):
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    account_name = models.CharField("계좌명", max_length=40)
+    account_num = models.PositiveBigIntegerField("계좌번호")
+    starting_fund = models.PositiveBigIntegerField("투자원금", default=0)
+    total_asset = models.PositiveBigIntegerField("계좌 총 자산", default=0)
+    isin = models.ForeignKey("investment.Stock", on_delete=models.CASCADE)
+    cur_price = models.PositiveBigIntegerField("현재가")
+    order = models.PositiveIntegerField("보유수량")
+
+    def __str__(self):
+        return self.account_name, self.cur_price, self.order
+
