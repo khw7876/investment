4bfd393ec8849d6954f9baf51e3d5851b6e7dd48
khw7876 <skanahrwi@naver.com>
2022-09-20 13:09:29 +0900

    feat/ account_asset_basic_csv 데이터 파일 모델에 반영

--- investment_service/investment/views.py
+++ investment_service/investment/views.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -9,15 +10,16 @@ from investment.models import (
     Investment as InvestmentModel,
     InvestmentHistory as InvestmentHistoryModel
     )
-import pandas as pd
 
 
 # Create your views here.
 class InvetmentView(APIView):
     
     def get(self, request):
-        data = pd.read_csv('investment_service/account_asset_info_set.csv')
-        username_data = data["고객이름"]
+        account_asset_data = pd.read_csv('investment_service/account_asset_info_set.csv')
+
+        # 여기는 accoutn_accet_info_set.csv 파일을 정제하는 데이터
+        username_data = account_asset_data["고객이름"]
         username_list = []
         for username in username_data:
             if not username in username_list:
@@ -25,7 +27,7 @@ class InvetmentView(APIView):
         for username in username_list:
             UserModel.objects.get_or_create(username = username)
 
-        bank_data = data["증권사"]
+        bank_data = account_asset_data["증권사"]
         bank_list = []
         for bank_name in bank_data:
             if not bank_name in bank_list:
@@ -33,11 +35,11 @@ class InvetmentView(APIView):
         for bankname in bank_list:
             BankModel.objects.get_or_create(name=bankname)
 
-        account_name_data = data["계좌명"]
-        account_num_data = data["계좌번호"]
-        isin_data = data["ISIN"]
-        cur_price_data = data["현재가"]
-        order_data = data["보유수량"]
+        account_name_data = account_asset_data["계좌명"]
+        account_num_data = account_asset_data["계좌번호"]
+        isin_data = account_asset_data["ISIN"]
+        cur_price_data = account_asset_data["현재가"]
+        order_data = account_asset_data["보유수량"]
         
         for index, account_num in enumerate(account_num_data):
             user = UserModel.objects.get(username=username_data[index])
@@ -58,6 +60,17 @@ class InvetmentView(APIView):
                 cur_price = cur_price_data[index],
                 order = order_data[index],
                 )
+        
+        # 여기부터는 account_asset_basic을 정제하는 데이터 
+        account_basic_data = pd.read_csv('investment_service/account_basic_info_set.csv')
+        account_num_data = account_basic_data["계좌번호"]
+        starting_fund_data = account_basic_data["투자원금"]
+
+        for index, account_num in enumerate(account_num_data):
+            investment_obj = InvestmentModel.objects.get(account_num = account_num_data[index])
+            investment_obj.starting_fund = starting_fund_data[index]
+            investment_obj.save()
+
 
         return Response({"detail" : "데이터를 성공적으로 저장하였습니다."}, status=status.HTTP_200_OK)
         
