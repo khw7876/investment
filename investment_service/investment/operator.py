4164b082b51ac9c72665f67f5de9850246328a61
khw7876 <khw7876@gmail.com>
2022-09-22 19:11:51 +0900

    feat/ aps-scheduler 완료

--- investment_service/investment/operator.py
+++ investment_service/investment/operator.py
@@ -3,8 +3,6 @@ from django_apscheduler.jobstores import register_events, DjangoJobStore
 from investment.views import make_data
 
 def start():
-    scheduler=BackgroundScheduler(timezone='Asia/Seoul')
-    @scheduler.scheduled_job('cron', hour = '23', name = 'make_data')
-    def auto_check():
-        make_data()
+    scheduler = BackgroundScheduler(timezone='Asia/Seoul')
+    scheduler.add_job(make_data , 'cron', hour = '09')
     scheduler.start()
--- investment_service/investment/views.py
+++ investment_service/investment/views.py
@@ -1,10 +1,4 @@
 import pandas as pd
-import datetime
-
-from rest_framework import status
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from user.models import User as UserModel
 from investment.models import (
